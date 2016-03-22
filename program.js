cdeebbbc9250f1b9c3e036510547ac431270c26f
Worth Baker <wbaker@greenmtd.com>
2016-03-22 15:13:20 -0600

    added official http juggler solution as a comment

--- program.js
+++ program.js
@@ -15,8 +15,6 @@ urls.forEach(function(url) {
     response.on('error', console.error);
 
     response.on("end", function() {
-      // console.log(stringData.length);
-      // console.log(stringData);
       responses[url] = stringData;
 
       if (Object.keys(responses).length === urls.length) {
@@ -27,3 +25,35 @@ urls.forEach(function(url) {
     });
   });
 });
+
+/*
+official solution
+
+var http = require('http')
+var bl = require('bl')
+var results = []
+var count = 0
+
+function printResults () {
+  for (var i = 0; i < 3; i++)
+    console.log(results[i])
+}
+
+function httpGet (index) {
+  http.get(process.argv[2 + index], function (response) {
+    response.pipe(bl(function (err, data) {
+      if (err)
+        return console.error(err)
+
+      results[index] = data.toString()
+      count++
+
+      if (count == 3)
+        printResults()
+    }))
+  })
+}
+
+for (var i = 0; i < 3; i++)
+  httpGet(i) 
+*/
