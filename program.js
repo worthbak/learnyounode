424900f18916884d8338c7d43890177e4e64d77a
Worth Baker <wbaker@greenmtd.com>
2016-03-22 14:23:57 -0600

    http collect passed w/out 3rd party packages

--- program.js
+++ program.js
@@ -4,16 +4,15 @@ var url = process.argv[2];
 
 http.get(url, function(response) {
   response.setEncoding("utf8");
+  var stringData = ""
 
-  response.on("data", function(data) {
-    console.log(data);
+  response.on('data', function(data) {
+    stringData += data
   });
+  response.on('error', console.error);
 
-  /*
-  official solution:
-    response.setEncoding('utf8')
-    response.on('data', console.log)
-    response.on('error', console.error) 
-  */
-
+  response.on("end", function() {
+    console.log(stringData.length);
+    console.log(stringData);
+  });
 });
