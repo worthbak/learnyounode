c834ec4e951cb3930616db084515d5c651ad6c49
Worth Baker <wbaker@greenmtd.com>
2016-03-22 15:12:17 -0600

    async juggler pased

--- program.js
+++ program.js
@@ -1,12 +1,29 @@
-var http = require('http')
-var bl = require('bl')
+var http = require('http');
 
-http.get(process.argv[2], function (response) {
-  response.pipe(bl(function (err, data) {
-    if (err)
-    return console.error(err)
-    data = data.toString()
-    console.log(data.length)
-    console.log(data)
-  }))
-})
+var urls = process.argv.splice(2);
+var responses = {};
+
+urls.forEach(function(url) {
+
+  http.get(url, function(response) {
+    response.setEncoding("utf8");
+    var stringData = ""
+
+    response.on('data', function(data) {
+      stringData += data
+    });
+    response.on('error', console.error);
+
+    response.on("end", function() {
+      // console.log(stringData.length);
+      // console.log(stringData);
+      responses[url] = stringData;
+
+      if (Object.keys(responses).length === urls.length) {
+        urls.forEach(function(item) {
+          console.log(responses[item]);
+        });
+      }
+    });
+  });
+});
