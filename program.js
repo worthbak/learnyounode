9a600118fe1ea078de1fd561bda675338a900a47
Worth Baker <wbaker@greenmtd.com>
2016-03-23 12:28:22 -0600

    http file server solution

--- program.js
+++ program.js
@@ -1,39 +1,19 @@
-var net = require('net');
-var datehelper = require('./modules/datehelper.js');
+// Requires
+var http = require('http');
+var fs = require('fs');
 
+// Process argument variables
 var arguments = process.argv.splice(2);
 var port = arguments[0];
+var filePath = arguments[1];
 
-var server = net.createServer(function(socket) {
-  // write "2013-07-06 17:42" time format
-  socket.write(datehelper.getCurrentFormattedDate() + "\n");
+var server = http.createServer(function (req, res) {
+  // official solution included this line; was the same otherwise
+  // res.writeHead(200, { 'content-type': 'text/plain' })
 
-  socket.end();
+  var fileStream = fs.createReadStream(filePath);
+
+  fileStream.pipe(res);
 });
 
 server.listen(port);
-
-/*
-official solution
-
-var net = require('net')
-
-function zeroFill(i) {
-  return (i < 10 ? '0' : '') + i
-}
-
-function now () {
-  var d = new Date()
-  return d.getFullYear() + '-'
-    + zeroFill(d.getMonth() + 1) + '-'
-    + zeroFill(d.getDate()) + ' '
-    + zeroFill(d.getHours()) + ':'
-    + zeroFill(d.getMinutes())
-}
-
-var server = net.createServer(function (socket) {
-  socket.end(now() + '\n')
-})
-
-server.listen(Number(process.argv[2])) 
-*/
